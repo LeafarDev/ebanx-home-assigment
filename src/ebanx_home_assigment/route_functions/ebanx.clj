50f6c6ef88fabd50a035924f3bedcd662f6cccf2
ew <rafael.o@aluno.ifsp.edu.br>
2020-08-13 20:29:38 -0300

    function increase/decrease function and transfer feature

--- src/ebanx_home_assigment/route_functions/ebanx.clj
+++ src/ebanx_home_assigment/route_functions/ebanx.clj
@@ -16,6 +16,17 @@
       (not-found! "0")
       (ok (str (:balance account))))))
 
+(defn- decrease-account-balance
+  [account-id amount]
+  (reset! accounts
+          (conj (filter #(not= account-id (:id %)) @accounts)
+                {:id account-id :balance (- (:balance (find-account (bigint account-id))) amount)})))
+
+(defn- increase-account-balance
+  [account-id amount]
+  (reset! accounts (conj (filter #(not= account-id (:id %)) @accounts)
+                         {:id account-id :balance (+ amount (:balance (find-account (bigint account-id))))})))
+
 (defn- deposit
   "Create account with initial balance
   POST /event {\"type\":\"deposit\", \"destination\":\"100\", \"amount\":10}
@@ -31,26 +42,48 @@
     (if (nil? account)
       (do (reset! accounts (conj @accounts {:id account-id :balance amount}))
           (created "" {:destination (find-account account-id)}))
-
-      (do (reset! accounts
-                  (conj (filter #(not= account-id (:id %)) @accounts)
-                        {:id account-id :balance (+ amount (:balance account))}))
+      (do (increase-account-balance account-id amount)
           (created "" {:destination (find-account account-id)})))))
 
 (defn- withdraw
-  "Withdraw from existing account
+  "Withdraw from non-existing accoun
+  POST /event {\"type\":\"withdraw\", \"origin\":\"200\", \"amount\":10}
+  404 0
+
+  Withdraw from existing account
   POST /event {\" type \":\" withdraw \", \" origin \":\" 100 \", \" amount \":5}
   201 {\" origin \": {\" id \":\" 100 \", \" balance \":15}}"
   [data]
   (let [account-id (bigint (:origin data))
         account (find-account account-id)
         amount (bigint (:amount data))]
-   (if (nil? account)
-     (not-found! "0")
-     (do (reset! accounts
-                (conj (filter #(not= account-id (:id %)) @accounts)
-                      {:id account-id :balance (- (:balance account) amount)}))
-        (created "" {:destination (find-account account-id)})))))
+    (if (nil? account)
+      (not-found! "0")
+      (do (decrease-account-balance account-id amount)
+          (created "" {:destination (find-account account-id)})))))
+
+(defn- transfer
+  "Transfer from existing account
+  POST /event {\"type\":\"transfer\", \"origin\":\"100\", \"amount\":15, \"destination\":\"300\"}
+  201 {\"origin\": {\"id\":\"100\", \"balance\":0}, \"destination\": {\"id\":\"300\", \"balance\":15}}
+
+  Transfer from non-existing account
+  POST /event {\"type\":\"transfer\", \"origin\":\"200\", \"amount\":15, \"destination\":\"300\"}
+  404 0"
+  [data]
+  (let [origin-account-id (bigint (:origin data))
+        origin-account (find-account origin-account-id)
+        destination-account-id (bigint (:destination data))
+        destination-account (find-account destination-account-id)
+        amount (bigint (:amount data))]
+    (if (nil? origin-account)
+      (not-found! "0")
+      (do (decrease-account-balance origin-account-id amount)
+          (if (nil? destination-account)
+            (reset! accounts (conj @accounts {:id destination-account-id :balance amount}))
+            (increase-account-balance destination-account-id amount))
+          (created "" {:origin      (find-account origin-account-id)
+                       :destination (find-account destination-account-id)})))))
 
 (defn event
   [request]
@@ -60,6 +93,8 @@
           (deposit data)
           (= type "withdraw")
           (withdraw data)
+          (= type "transfer")
+          (transfer data)
           :else
           (bad-request "invalid type"))))
 
